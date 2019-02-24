3fab0ad46c6791e76662678a5def8ca8e91f84db
Saran Sundararajan <saran@Sarans-MacBook-Pro-2.local>
2019-02-23 21:23:52 -0600

    Responses Complete, data available to filter

--- Server/api.py
+++ Server/api.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
-CORS(app, supports_credentials=True, resources=r'/api/*')
+CORS(app, supports_credentials=True)
 app.config.update(
     DEBUG=True,
     JSON_SORT_KEYS=False,
@@ -24,7 +24,7 @@ loans_endpoint = "https://api.theexchange.fanniemae.com/v1/manufactured-housing-
 
 # [GET] /loans
 # Retrieves and sends loan information from the Fannie Mae API
-@app.route("/api/v1/get-loans/")
+@app.route("/api/get-loans/")
 def get_loans():
     # Retrieve data from the Fannie Mae API loans endpoint
     headers = {'Authorization': getenv('USER_TOKEN')}
@@ -36,32 +36,35 @@ def get_loans():
 
 # [GET] /loans
 # Retrieves and sends loan information from the Fannie Mae API
-@app.route("/api/v1/get-counties/<state_id>")
+@app.route("/api/get-counties/<state_id>")
 def get_counties(state_id):
     endpoint = "https://search.onboard-apis.com/areaapi/v2.0.0/county/lookup?StateId=ST" + state_id
     headers = {'apikey': getenv('API_KEY'), 'accept': 'application/json'}
     app.logger.info(session.get("state_ids", []))
     response = get(endpoint, headers=headers)
+    data = response.json()
+    data = data["response"]["result"]["package"]["item"]
     # Proxy the data response to the original request
-    return jsonify(response.json())
+    return jsonify(data)
 
 
-@app.route("/api/v1/get-state-ids/")
+@app.route("/")
 def get_state_ids():
+    app.logger.info('Getting State ID\'s')
     endpoint = "https://search.onboard-apis.com/areaapi/v2.0.0/state/lookup"
     headers = {'apikey': getenv('API_KEY'), 'accept': 'application/json'}
     response = get(endpoint, headers=headers)
-    state_ids = []
     if response.status_code != 200:
-        print("ERROR")
+        app.logger.error('Could not retrieve State ID\'s')
         state_ids = []
     else:
         data = response.json()
         state_ids = data["response"]["result"]["package"]["item"]
+        app.logger.info('State ID\'s Retrieved')
     session["state_ids"] = state_ids
-    app.logger.info('State ID\'s Retrieved')
     return jsonify(state_ids)
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 8000), debug=True)
+    get_state_ids()
