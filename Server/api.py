e1aa560cf1b263704851bc8f41434966cea6b73d
Saran Sundararajan <saran@Sarans-MacBook-Pro-2.local>
2019-02-23 18:59:27 -0600

    Server updates

--- Server/api.py
+++ Server/api.py
@@ -1,13 +1,21 @@
+import os
 from os import getenv
 
 from dotenv import load_dotenv
 from flask import Flask, jsonify
+from flask_cors import CORS
 from requests import get
 
 # Load environment variables
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True, resources=r'/api/*')
+app.config.update(
+    DEBUG=True,
+    JSON_SORT_KEYS=False,
+    JSONIFY_PRETTYPRINT_REGULAR=False
+)
 
 # The endpoint for retrieving loan information from the Fannie Mae API, ordered by state
 loans_endpoint = "https://api.theexchange.fanniemae.com/v1/manufactured-housing-loans/calculations-by-state"
@@ -15,7 +23,7 @@ loans_endpoint = "https://api.theexchange.fanniemae.com/v1/manufactured-housing-
 
 # [GET] /loans
 # Retrieves and sends loan information from the Fannie Mae API
-@app.route("/loans")
+@app.route("/api/v1/get-loans/")
 def get_loans():
     # Retrieve data from the Fannie Mae API loans endpoint
     headers = {'Authorization': getenv('USER_TOKEN')}
@@ -26,4 +34,4 @@ def get_loans():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 8000), debug=True)
